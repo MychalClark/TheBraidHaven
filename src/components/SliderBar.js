33325c2eb8e639ebdc5075f410e34939ab918955
Mychal Clark <54602958+MychalClark@users.noreply.github.com>
2023-10-17 03:29:38 -0500

    SliderBars Sliding

--- src/components/SliderBar.js
+++ src/components/SliderBar.js
@@ -1,15 +1,49 @@
 import "../css/sliderBar.css";
-function SliderBar() {
+import "../index.css";
+import { useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
+
+function SliderBar({ backgroundColor, title, desc, img, link }) {
+  const randomDuration = Math.random() * (900 - 500) + 500;
+  const slideIn = useSpring({
+    from: { transform: "translateX(100%)" },
+    to: { transform: "translateX(0)" },
+    config: { duration: randomDuration },
+  });
+  const fadeIn = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: randomDuration },
+  });
   return (
-    <div className="sliderBar">
-      <div className="sliderText">
-        <h3 className="sliderTitle">Shop New Deals</h3>
-        <span className="sliderText">Deal on box braids as supplies last.</span>
+    <animated.div
+      style={{
+        ...slideIn,
+        ...fadeIn,
+      }}
+      className={`sliderBar d-flex ${backgroundColor}`}
+    >
+      <div className="sliderText col-7 d-flex flex-column align-items-center ">
+        <a
+          name=""
+          id=""
+          class="btn sliderTitle passionFont textColor2 m-0 p-0 h-50 align-items-end"
+          href="#"
+          role="button"
+        >
+          <h2 className="sliderTitle passionFont textColor2">{title}</h2>
+        </a>
+        <div className="d-flex flex-column justify-content-top  h-50">
+          <span className="sliderText textColor2 fairFont text-center">
+            {desc}
+          </span>
+        </div>
       </div>
-      <div className="sliderImgContainer">
-        <img className="sliderImg" src=""></img>
+      <div className="sliderImgContainer col-5">
+        <img className="sliderImg" src={img} alt="Slider Image" />
       </div>
-    </div>
+    </animated.div>
   );
 }
+
 export default SliderBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import SliderBar from "../components/SliderBar";
 import { Container } from "react-bootstrap";
+import "../index.css";
 function Home() {
   return (
     <div className="home">
-      <SliderBar />
-      <SliderBar />
-      <SliderBar />
+      <SliderBar
+        backgroundColor="backgroundColor1"
+        title="Schedule!"
+        desc="Find openings, get yer hair done."
+        img="/images/girl_thumbnail.png"
+      />
+      <SliderBar
+        backgroundColor="backgroundColor4"
+        title="DEALS"
+        desc="New Deals. Shop fast or dont."
+        img="/images/girl1.png"
+      />
+      <SliderBar
+        backgroundColor="backgroundColor6"
+        title="Gallery"
+        desc="Find the hair thats right for you or sumn."
+        img="/images/girl_thumbnail.png"
+      />
     </div>
   );
 }
